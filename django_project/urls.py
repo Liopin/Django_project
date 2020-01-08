dfffee5cb0d87bca84d027f7f20365f9e5cb35af
Liopin <contact.pinheiro.lionel@gmail.com>
2020-01-08 10:33:50 +0100

    user profile and picture

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as users_views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +29,7 @@ urlpatterns = [
     path('profile/', users_views.profile, name='profile'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
